ba700814ecefddfcb75df2a262443f1f0f548745
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-19 11:17:01 +0100

    changes

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './topbar.css'
+import instagram from '../../images/social/instagram.png'
 
 
 const Topbar = () => {
@@ -7,16 +8,42 @@ const Topbar = () => {
     <div data-aos='fade-down' className='topbar'>
         <div className="topbarWrapper">
             <div className="topbarWrapperLeft">
+                <IconLink
+                 link={"https://www.instagram.com/click.e.sport/"}
+                 image={instagram}
+                />
+                 <IconLink
+                 link={"https://www.instagram.com/click.e.sport/"}
+                 image={instagram}
+                />
+                 <IconLink
+                 link={"https://www.instagram.com/click.e.sport/"}
+                 image={instagram}
+                />
+                 <IconLink
+                 link={"https://www.instagram.com/click.e.sport/"}
+                 image={instagram}
+                />
+            </div>
+            <div data-aos='fade-down' className="topbarWrapperCenter">
 
             </div>
-            <div className="topbarWrapperCenter">
-
-            </div>
-            <div className="topbarWrapperRight">
+            <div data-aos='fade-down' className="topbarWrapperRight">
 
             </div>
         </div>
     </div>
   )
 }
+
+const IconLink = ({link,image}) =>{
+    return (
+        <a href={link} target='_blank' className="socialIconLink">
+        <img src={image} alt="" className="socialIcon" />
+    </a>
+    )
+}
+
+
+
 export default Topbar
