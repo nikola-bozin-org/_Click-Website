8748d9ebc4d2586e54842385dc5d27b1b0971522
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-19 10:21:31 +0100

    first push

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './topbar.css'
+
+
+const Topbar = () => {
+  return (
+    <div data-aos='fade-down' className='topbar'>
+        <div className="topbarWrapper">
+            <div className="topbarWrapperLeft">
+
+            </div>
+            <div className="topbarWrapperCenter">
+
+            </div>
+            <div className="topbarWrapperRight">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Topbar
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '../components/Topbar/Topbar'
+
+const Homepage = () => {
+
+  return (
+    <div>
+    <div>
+      <Topbar/>
+    </div>
+    </div>
+  )
+}
+
+export default Homepage
