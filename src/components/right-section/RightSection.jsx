6fa66d2479ebbaf6f16bdf3cef32534e7651b1a0
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-22 20:54:53 +0100

    changes + right section

--- src/components/right-section/RightSection.jsx
+++ src/components/right-section/RightSection.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './rightSection.css'
 
-const RightSection = () => {
+const RightSection = ({image,title,text}) => {
   return (
-    <div className='rightSection'>RightSection</div>
+    <div data-aos='fade-up' className='rightSection'>
+      <img data-aos-delay='500' data-aos='fade-right' src={image} alt="" className="rightSectionBackgroundImage" />
+      <div className="rightSectionBackground">
+        <div className="rightSectionBackgroundTexts">
+          <p className="rightSectionBackgroundTitle">{title}</p>
+          <p className="rightSectionBackgroundText">{text}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,10 @@ import LineGradient from '../components/line-gradient/LineGradient'
 import LeftSection from '../components/left-section/LeftSection'
 import RightSection from '../components/right-section/RightSection'
 
+import click1 from '../images/click/click1.png'
+import click2 from '../images/click/click2.png'
 import click3 from '../images/click/click3.png'
+import click4 from '../images/click/click4.png'
 
 const Homepage = () => {
 
@@ -22,15 +25,41 @@ const Homepage = () => {
         <Intro/>
         <Information/>
         <LineGradient/>
-        <LeftSection image={click3} title={"Cekamo te"}
-         text={`da ostvaris svoje najbolje gejming trenutke!
+        <LeftSection 
+        image={click1}
+        title={"Cekamo te"}
+        text={`da ostvaris svoje najbolje gejming trenutke!
                 Otvoreni smo svakog dana od 10:00 do 03:00. 
                 Sve te ceka spremno!
                 Vezuj se, dzojstik u ruke i polecemo!`
                 }/>
         <LineGradient/>
-        {/* <RightSection/>
-        <LineGradient/> */}
+        <RightSection
+        image={click2}
+        title={"Click Esports Dota 2"}
+        text={`je uzbudljiva nocna akcija gejmera  nepokolebljivog mindset-a!
+        Sa timom od 5 igraca, osvoji posebne benefite Click-a, i zauzmite svoju poziciju kao jedan od najboljih timova ove neverovatne igre!
+        Nemoj da propustis ultimativno Dota 2 iskustvo!`
+                }/>
+        <LineGradient/>
+        <LeftSection 
+        image={click3}
+        title={"Turniri"}
+        text={`Podigni svoje gejming iskustvo na profesionalni nivo.
+        Upoznaj novo drustvo u Click-u, napravite tim, i zakoracite u svet neprestane borbe za pobedom!
+        Pro Room Click-a je spreman da uz mocnu opremu ti omoguci da je tvoja jedina briga da pobedis partiju!`
+                }/>
+        <LineGradient/>
+        <RightSection
+        image={click4}
+        title={"Rodjendani"}
+        text={`Tvoj dan je dosao! Ucini ga jedinstvenim i nezaboravnim!
+        Proslavi rodjendan na gejmerski nacin!
+        Zapocni svoju rodjendansku zabavu i zakoraci u elitne prostorije Click Esport-a.
+        Budi MVP i lider tvog rodjendana i pokazi svima sta znaci biti PRO!
+        Nezaboravna secanja i najlepsa druzenja te cekaju!`
+                }/>
+        <LineGradient/>
         <SiteInDevelopment/>
     </div>
     </>
