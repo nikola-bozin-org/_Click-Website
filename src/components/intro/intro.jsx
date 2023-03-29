47e05cd2a4f49ea0b3fb65aeee77bf1d822b20fa
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-29 08:53:40 +0200

    /

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import './intro.css'
+import nbLogo from '../../images/click/nikola-bozin-logo.png'
+import vaLogo from '../../images/click/view-apartments-logo.png'
 import clickLogo from '../../images/click/clickLogo.png'
-import clickNekretnine from '../../images/click/clickNekretnine.png'
 
 const Intro = () => {
   return (
     <div className='intro'>
-      <div data-aos-delay="300" data-aos="fade-up" className="poweredBy">
-        {/* <p className="poweredByText">Powered by:</p> */}
-         {/* <img src={clickNekretnine} alt="" className="poweredByImage" /> */}
-         {/* <img src={clickLogo} alt="" className="poweredByImage" />  */}
-      </div>
         <p data-aos-delay="500" data-aos="fade-up" className="click">
             Click E-Sports
         </p>
         <img  data-aos-delay="800" data-aos="fade-up" src={clickLogo} alt="" className="clickLogo" />
         <p  data-aos-delay="800" data-aos="fade-up" className="ultimativni">Ultimativni E-Sports Centar!</p>
+        <div data-aos-delay="1100" data-aos="fade-in" className="poweredBy">
+        <p  className="poweredByText">Powered by:</p>
+        <div className="powerByImages">
+         <img src={nbLogo} alt="" className="poweredByImage" />
+         <img src={vaLogo} alt="" className="poweredByImage" /> 
+        </div>
+      </div>
     </div>
   )
 }
