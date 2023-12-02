900e56ddecdac3da0778d4ffc4b9ce24ef4f5fbb
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-12-02 21:09:53 +0100

    /

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css'
 import nbLogo from '../../images/click/nikola-bozin-logo.png'
-import vaLogo from '../../images/click/view-apartments-logo.png'
+import vaLogo from '../../images/click/view-apartments-logo.png';
 import oxford from '../../images/click/oxford.png'
 const Footer = () => {
 
@@ -15,8 +15,8 @@ const Footer = () => {
         <div className="poweredBy">
         <p  className="poweredByText">Powered by:</p>
         <div className="powerByImages">
-         <img onClick={(e)=>{navigate(e,"https://www.nikolabozin.com")}} src={nbLogo} alt="" className="poweredByImage nikolaBozin" />
-         <img onClick={(e)=>{navigate(e,"https://www.instagram.com/stan.na.dan.nis")}} src={vaLogo} alt="" className="poweredByImage vaLogo" /> 
+         <img onClick={(e)=>{navigate(e,"https://www.nikolabozin.org")}} src={nbLogo} alt="" className="poweredByImage nikolaBozin" />
+         <img onClick={(e)=>{navigate(e,"https://www.instagram.com/mar_residence")}} src={vaLogo} alt="" className="poweredByImage vaLogo" /> 
          <img onClick={(e)=>{navigate(e,"https://www.oxford.rs")}} src={oxford} alt="" className="poweredByImage oxfordLogo" /> 
         </div>
       </div>
