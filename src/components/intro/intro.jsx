0b9a2a6a0fce0bc59e746f0258742ac99313a3a9
Nikola Bozin <112718743+nikola-bozin-org@users.noreply.github.com>
2025-04-25 13:57:13 +0200

    Update intro.jsx

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -12,8 +12,8 @@ const Intro = () => {
         <img  data-aos-delay="800" data-aos="fade-up" src={clickLogo} alt="" className="clickLogo" />
         <div className='last'>
         <p style={{textAlign:'center'}} data-aos-delay="800" data-aos="fade-up" className="ultimativni">Ultimativni</p>
-        <p  style={{textAlign:'center'}} data-aos-delay="800" data-aos="fade-up" className="ultimativni">E-Sports</p>
-        <p  style={{textAlign:'center'}} data-aos-delay="800" data-aos="fade-up" className="ultimativni">Centar!</p>
+        <p  style={{textAlign:'center'}} data-aos-delay="100" data-aos="fade-up" className="ultimativni">E-Sports</p>
+        <p  style={{textAlign:'center'}} data-aos-delay="100" data-aos="fade-up" className="ultimativni">Centar!</p>
         </div>
     </div>
   )
